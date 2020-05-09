1f55cbf278a5eaa0f04106199f4b204fa6d83f25
mor <morovadia@mail.tau.ac.il>
2020-05-09 23:10:46 +0300

    9/15

--- hw5.py
+++ hw5.py
@@ -20,8 +20,8 @@ class QuestionnaireAnalysis:
     def show_age_distrib(self):
     # def show_age_distrib(self) -> Tuple[np.ndarray, np.ndarray]:
         df = self.data
-        hist, bins = np.histogram(df.age.dropna(), bins = [1,10,20,30,40,50,60,70,80,90,100])
-        return (hist,bins)
+        hist, bins = np.histogram(df['age'], bins = [0,10,20,30,40,50,60,70,80,90,100])
+        return hist, bins
 #Q2
     def remove_rows_without_mail(self) -> pd.DataFrame:
 
@@ -31,8 +31,9 @@ class QuestionnaireAnalysis:
             if loc_char == -1 or arg[loc_char + 1] == '.':
                 arg = np.nan
             
-        df_filtered = df_unfiltered.email.apply(is_invalid_email, axis=1)
-        df = df_filtered.dropna(subset=['email']).reset_index(drop=True)  
+        df_filtered = df_unfiltered.email.apply(lambda x: is_invalid_email(x))
+        df_unfiltered['email'] = df_filtered.dropna()
+        df = df_unfiltered.reset_index()
         return df
 
 #Q3
@@ -42,17 +43,20 @@ class QuestionnaireAnalysis:
         d_ans = df[['q1','q2','q3','q4','q5']] 
         NaN_only = d_ans.isnull().any(axis=0)
         arr = NaN_only.index.values
-        df = df.fillna(df.mean(subset=['q1','q2','q3','q4','q5'], axis=0))
-        return tuple(df,arr)     
+        df = df.fillna(d_ans.mean(axis=0))
+        return (df,arr)     
 
 #Q4
     def score_subjects(self, maximal_nans_per_sub: int = 1) -> pd.DataFrame:
 
         df = self.data
-        d_ans = df[['q1','q2','q3','q4','q5']] 
+        d_ans = df[['q1','q2','q3','q4','q5']]
         d_ans = d_ans.fillna(d_ans.mean(axis=1),limit = maximal_nans_per_sub)
         mean_grade = d_ans.mean(axis=0).round(0).astype('UInt8')
         df['score'] = mean_grade
         return df
 
 #Q5 BONUS
+    # def correlate_gender_age(self) -> pd.DataFrame:
+    #     df = self.data
+    #     idx = pd.MultiIndex.from_product([df.index],['gender'],['age'])
