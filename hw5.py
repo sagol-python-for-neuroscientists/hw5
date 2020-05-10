03a14aec91b1b4c3f383eaccaa53c545838969de
mor <morovadia@mail.tau.ac.il>
2020-05-10 23:44:31 +0300

    11/15

--- hw5.py
+++ hw5.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
+import re
 
 
 class QuestionnaireAnalysis:
@@ -19,44 +20,35 @@ class QuestionnaireAnalysis:
 #Q1
     def show_age_distrib(self):
     # def show_age_distrib(self) -> Tuple[np.ndarray, np.ndarray]:
+
         df = self.data
         hist, bins = np.histogram(df['age'], bins = [0,10,20,30,40,50,60,70,80,90,100])
         return hist, bins
 #Q2
     def remove_rows_without_mail(self) -> pd.DataFrame:
 
-        df_unfiltered = self.data
-        def is_invalid_email(arg):  #check if email address is invalid
-            loc_char = arg.find('@')
-            if loc_char == -1 or arg[loc_char + 1] == '.':
-                arg = np.nan
-            
-        df_filtered = df_unfiltered.email.apply(lambda x: is_invalid_email(x))
-        df_unfiltered['email'] = df_filtered.dropna()
-        df = df_unfiltered.reset_index()
-        return df
+        df = self.data
+        df = df[df['email'].str.contains(r'[^@]+@[^@]+\.[^@]+')]
+        return df.reset_index()
 
 #Q3
     # def fill_na_with_mean(self) -> Tuple[pd.DataFrame, np.ndarray]:
     def fill_na_with_mean(self):
+        
         df = self.data
-        d_ans = df[['q1','q2','q3','q4','q5']] 
-        NaN_only = d_ans.isnull().any(axis=0)
-        arr = NaN_only.index.values
-        df = df.fillna(d_ans.mean(axis=0))
-        return (df,arr)     
+        mean_row = df[['q1','q2','q3','q4','q5']].mean(axis=1)
+        arr = df.dropna(subset = ['q1','q2','q3','q4','q5']).index.values
+        idxna = np.array([i for i in list(df.index.values) if i not in arr]) 
+        df[['q1','q2','q3','q4','q5']] = df[['q1','q2','q3','q4','q5']].fillna(mean_row[arr])
+        return df , idxna
 
 #Q4
     def score_subjects(self, maximal_nans_per_sub: int = 1) -> pd.DataFrame:
-
         df = self.data
-        d_ans = df[['q1','q2','q3','q4','q5']]
-        d_ans = d_ans.fillna(d_ans.mean(axis=1),limit = maximal_nans_per_sub)
-        mean_grade = d_ans.mean(axis=0).round(0).astype('UInt8')
-        df['score'] = mean_grade
+        df_temp = df[['q1','q2','q3','q4','q5']]
+        mean_row = df_temp.mean(axis=1)
+        arr = df_temp.dropna().index.values
+        df_temp = df_temp.fillna(mean_row[arr] , limit = maximal_nans_per_sub).mean(axis = 1).apply(np.floor).astype('UInt8')
+        df['score'] = df_temp
         return df
-
-#Q5 BONUS
-    # def correlate_gender_age(self) -> pd.DataFrame:
-    #     df = self.data
-    #     idx = pd.MultiIndex.from_product([df.index],['gender'],['age'])
+        
