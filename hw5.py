f10f9cd26efc215b2fb59e0cf2d1d73d67fb4a5e
Nitzantr <nitzant@mail.tau.ac.il>
2020-05-12 17:45:20 +0300

    plt.show() hist

--- hw5.py
+++ hw5.py
@@ -37,6 +37,7 @@ class QuestionnaireAnalysis:
         age = age.dropna()
         hist, bins = np.histogram(age, bins = [0,10,20,30,40,50,60,70,80,90,100])
         plt.hist(age, bins = bins)
+        plt.show()
 
         return hist, bins
 
