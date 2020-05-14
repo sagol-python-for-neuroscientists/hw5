fixed wrong axis

hw5.py
@@ -71,7 +71,7 @@ class QuestionnaireAnalysis:
         df_q = df.loc[:, 'q1':'q5']
         rows,columns = np.where(df_q.isna())
         rows = np.unique(rows)
-        df_q = df_q.fillna(df_q.mean(axis=0))
+        df_q = df_q.fillna(df_q.mean(axis=1))
         df.loc[:, 'q1':'q5'] = df_q
         
         return df , rows