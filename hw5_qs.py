1fecd2b3d045292df6aa4dbcc15e6f0381ec72d4
Nina_Besser_Ilan <nina.besser.ilan@gmail.com>
2023-05-26 16:57:10 +0300

    finish q1 (passed test)

--- hw5_qs.py
+++ hw5_qs.py
@@ -2,7 +2,9 @@ from typing import Union
 import numpy as np
 import pathlib
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
+
+
 
 
 class QuestionnaireAnalysis:
@@ -19,6 +21,37 @@ class QuestionnaireAnalysis:
     def read_data(self):
         """Reads the json data located in self.data_fname into memory, to the attribute self.dat """
         self.dat = pd.read_json(self.data_fname)
+
+    
+    def show_age_distrib(self) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Calculates and plots the age distribution of the participants. 
+        The bins for the histogram should be [0, 10), [10, 20), [20, 30), ..., [90, 100]
+
+   Returns
+   -------
+   hist : np.ndarray
+     Number of people in a given bin
+   bins : np.ndarray
+     Bin edges
+        
+        
+        
+        """
+        self.dat = self.dat.dropna(subset=["age"])
+        bins = np.arange(0, 101, 10)
+        counts, edges, plot = plt.hist(self.dat['age'], bins)
+        plt.show()
+        return counts, edges
+
+
+
+        
+
+
+
+
+
 
         
 
