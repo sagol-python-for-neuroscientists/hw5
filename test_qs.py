014ef745672ef8c89a64350968ccf7de78bb658d
Noga Mendelovich <nogam2@mail.tau.ac.il>
2023-05-29 16:00:48 +0300

    test changed

--- test_qs.py
+++ test_qs.py
@@ -101,12 +101,13 @@ def test_score_dtype():
 
 
 def test_score_results():
-    truth = pd.read_csv('tests_data/q4_score.csv', squeeze=True, index_col=0).astype("UInt8")
+    truth = pd.read_csv('tests_data/q4_score.csv', index_col=0).astype("UInt8")
+    truth.squeeze()
     fname = 'data.json'
     q = QuestionnaireAnalysis(fname)
     q.read_data()
     df = q.score_subjects()
-    assert df["score"].equals(truth)
+    assert df["score"].equals(truth['score'])
 
 
 def test_correlation():
